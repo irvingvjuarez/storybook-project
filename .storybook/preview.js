6ea24c3e313b08c51f24b03f7b4e8a68be9138a0
Irving Juarez <Irving.Juarez@thomsonreuters.com>
2025-02-07 23:12:46 -0600

    Adding custom viewports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,22 @@
 /** @type { import('@storybook/html').Preview } */
 
+const customViewports = {
+  smallUgly: {
+    name: 'Small Ugly',
+    styles: {
+      width: '360px',
+      height: '540px',
+    },
+  },
+  mediumUgly: {
+    name: 'Medium Ugly',
+    styles: {
+      width: '1024px',
+      height: '1200px',
+    },
+  },
+}
+
 const preview = {
   parameters: {
     viewMode: "docs",
@@ -9,7 +26,10 @@ const preview = {
         date: /Date$/i,
       },
     },
-  },
+    viewport: {
+      viewports: customViewports,
+    },
+  }
 };
 
 export default preview;
